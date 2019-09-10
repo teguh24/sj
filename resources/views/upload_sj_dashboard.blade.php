81d8f4a8f2efd877bc41852f5c75e5b4b53c7922
exim <exim@aiia.co.id>
2019-09-10 13:53:12 +0700

    hot fix link upload

--- resources/views/upload_sj_dashboard.blade.php
+++ resources/views/upload_sj_dashboard.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-md-6 col-md-offset-3"  style="border: 4px solid #a1a1a1;">
         	<h2><center><font color="white">Upload SJ </font></center></h2>
-            <center><form action="http://172.18.10.135/sj/public/upload/sj/dashboard" class="form-horizontal" method="post" enctype="multipart/form-data">
+            <center><form action="{{asset('upload/sj/dashboard')}}" class="form-horizontal" method="post" enctype="multipart/form-data">
 			{{ csrf_field() }}
 			<input type="file" name="sj" /><br>
 			<button class="btn btn-primary">Proses</button>
